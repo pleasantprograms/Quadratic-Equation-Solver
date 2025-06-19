03c029a967d1aae79ad2fa369b3bccf110f4ce9b
pleasantprograms <priyanshu23nanda2k23@gmail.com>
2025-06-19 12:32:25 +0530

    Create Main Code.cpp

---
+++ Main Code.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main () {
+  
+  float a,b,c;
+  cout << "Enter the value of a" << endl;
+  cin >> a;
+  
+  cout << "Enter the value of b" << endl;
+  cin >> b;
+  
+  cout << "Enter the value of c" << endl;
+  cin >> c;
+
+  if (pow(b,2) >= 4*a*c) {
+   float p = (-b + (sqrt (pow(b,2) - 4 * a * c)))/(2 * a);
+   float q = (-b - (sqrt (pow(b,2) - 4 * a * c)))/(2 * a);
+   
+   cout << "The roots of your quadratic equation is/are " << p << " and " << q << endl;}
+  
+  else {cout << "The quadratic equation does not have real solutions" << endl;}
+}
